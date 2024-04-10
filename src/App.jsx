07b67a682108c0fd6aa167e337e39ba835e76bbf
Purva S <spurva449@gmail.com>
2024-04-10 19:47:28 +0530

    Commit 1 for map

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import axiosInstance from './axios_instance';
 import Header from './Header';
-
+import MapComponent from './MapComponent';
 
 function App() {
   const [data, setData] = useState(null);
@@ -18,40 +18,16 @@ function App() {
   }, []);
 
   return (
-<<<<<<< HEAD
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="src/assets/zoro.jpeg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-=======
+
     <div className="App">
        <Header />
+       < MapComponent />
        
       <h1>React Frontend</h1>
      
       {data && <p>{data.message}</p>}
     </div>
   );
->>>>>>> refs/remotes/origin/main
 }
 
 export default App;
