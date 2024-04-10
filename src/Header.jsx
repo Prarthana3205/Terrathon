2f83e6a3b7dff66a5bddfaa454d28b051fb3a290
Purva S <spurva449@gmail.com>
2024-04-10 19:26:01 +0530

    new commit

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 const Header = () => {
   return (
-    <div style={{backgroundImage: 'linear-gradient(to bottom, rgba(255, 255, 255, 0), white),url("https://images.pexels.com/photos/210288/pexels-photo-210288.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")', backgroundSize: 'cover', width: '100%', height: '600px',fontSize:'60px',textAlign:"center"}}>Welcome to Lake Vigil</div>
-  )
-}
+    <div
+      style={
+        {
+        backgroundImage: 'linear-gradient(to bottom, rgba(255, 255, 255, 0), white),url("https://i.pinimg.com/originals/29/5d/13/295d13ace364ebc92b368ff1ce63d55b.jpg")',
+        backgroundSize: '100% auto', 
+        width: '100%',
+        height: '100vh', 
+        margin: '0px',
+        padding:'0px',
+        display: 'flex',             // Enable Flexbox
+        justifyContent: 'center',    // Center horizontally
+        alignItems: 'center',   
+        fontSize: '80px',
+        fontFamily:'Bold',
+        textAlign: 'center',
+      }}
+    >
+      Welcome to LakeVigil
+    </div>
+  );
+};
 
-export default Header
+export default Header;
