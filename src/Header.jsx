1413be7634cde028bfd86d307edb0e39376cb79a
Purva S <spurva449@gmail.com>
2024-04-10 18:20:42 +0530

    commit 1

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div style={{backgroundImage: 'linear-gradient(to bottom, rgba(255, 255, 255, 0), white),url("https://images.pexels.com/photos/210288/pexels-photo-210288.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")', backgroundSize: 'cover', width: '100%', height: '600px',fontSize:'60px',textAlign:"center"}}>Welcome to Lake Vigil</div>
+  )
+}
+
+export default Header
